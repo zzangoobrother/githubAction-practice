97a369c8bf83ff4290233be468dfa5b999be2541
choiseon <com1368@naver.com>
2023-01-03 00:39:21 +0900

    add dynamic variable

--- src/main/java/dev/be/githubactionpractice/controller/DemoController.java
+++ src/main/java/dev/be/githubactionpractice/controller/DemoController.java
@@ -1,13 +1,17 @@
 package dev.be.githubactionpractice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Value("${override.value}")
+    private String value;
+
     @GetMapping("/demo")
     public String demo() {
-        return "demo";
+        return "demo " + value;
     }
 }
