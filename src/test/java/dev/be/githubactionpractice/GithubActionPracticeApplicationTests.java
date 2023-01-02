2f34326f23de078e2d8d9937816a8a026e8b56b0
choiseon <com1368@naver.com>
2023-01-03 00:07:19 +0900

    project setup

---
+++ src/test/java/dev/be/githubactionpractice/GithubActionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.githubactionpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
