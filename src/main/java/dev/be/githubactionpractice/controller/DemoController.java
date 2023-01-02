05fbfade775081c05bee57f0b3fcff2ea4565345
choiseon <com1368@naver.com>
2023-01-03 00:27:22 +0900

    add controller

---
+++ src/main/java/dev/be/githubactionpractice/controller/DemoController.java
@@ -0,0 +1,13 @@
+package dev.be.githubactionpractice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "demo";
+    }
+}
